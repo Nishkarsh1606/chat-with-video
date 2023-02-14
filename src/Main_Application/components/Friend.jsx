3905b6b0d8b7d0d4caadf7e51ca800744076ee34
Nishkarsh-Mac <nishkarsh2912@gmail.com>
2023-02-14 12:09:48 +0530

    feature: highlight current selected chat

--- src/Main_Application/components/Friend.jsx
+++ src/Main_Application/components/Friend.jsx
@@ -1,19 +1,37 @@
 import { Avatar } from '@mui/material'
 import React from 'react'
 import './Friend.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { selectChannel } from '../../AppSlice/AppSlice';
+
 
 function Friend({nameOfFriend,lastMessage,avatarURL, uid}) {
-  const select='uid'
-  return (
-
-    <div className="Friend">
-      <Avatar/>
-      <div className="message-info">
-        <p>{nameOfFriend}</p>
-        <p>{lastMessage}</p>
+  const currentChannel=useSelector((state)=>state.app.currentChannel)
+  const dispatch=useDispatch()
+  const highlightChannel=()=>{
+    dispatch(selectChannel(uid))
+  }
+  if(currentChannel!==uid){
+    return (
+      <div className="Friend" onClick={highlightChannel}>
+        <Avatar/>
+        <div className="message-info">
+          <p>{nameOfFriend}</p>
+          <p>{lastMessage}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }else{
+    return (
+      <div className="Friend Friend-highlighted" onClick={highlightChannel}>
+        <Avatar/>
+        <div className="message-info">
+          <p>{nameOfFriend}</p>
+          <p>{lastMessage}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Friend
